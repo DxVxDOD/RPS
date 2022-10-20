fa3111868b0fa4f59b78334c2367de48b2a2a7de
David Orbang <orbangdavid@yahoo.com>
2022-10-20 13:26:35 +0100

    yes

--- myscripts.js
+++ myscripts.js
@@ -5,7 +5,8 @@ function computerChoice (choice) {
     return choice[index];
 }
 
-let playerPrompt = prompt("Please chose Rock, Paper or Scissors!")
+
+
 
 function playerChoice (choicea) {
     if (choicea === "Rock") {
@@ -22,4 +23,27 @@ function playerChoice (choicea) {
     }
 }
 
-console.log(playerChoice(playerPrompt));
+
+function playRound(comp, player) {
+    if (comp === player) {
+        alert("It is a draw try again!");
+    } else if (comp === "Rock" && player === "Scissors") {
+        return "Computer has won the round!";
+    } else if (comp === "Rock" && player === "Paper") {
+        return "Player has won the round!";
+    } else if (comp === "Scissors" && player === "Paper") {
+        return "Computer has won the round!";
+    } else if (comp === "Scissors" && player === "Rock") {
+        return "Player has won the round!";
+    } else if (comp === "Paper" && player === "Rock") {
+        return "Comuter has won the round!";
+    } else if (comp === "Paper" && player === "Scissors") {
+        return "Player has won the round!";
+    }
+}
+
+
+for (let i = 0; i < 5; i++) {
+    let playerPrompt = prompt("Please chose Rock, Paper or Scissors!");
+    console.log(playRound(computerChoice(options), playerChoice(playerPrompt)));
+}
